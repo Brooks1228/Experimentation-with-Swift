5cf670ded8f9a308bba8fcbeaec6152fdb212263
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2025-10-15 13:46:03 -0400

    Initial Commit

---
+++ Wristcord Watch App/WristcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristcordApp.swift
+//  Wristcord Watch App
+//
+//  Created by Brooks on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Wristcord_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wristcord/WristcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristcordApp.swift
+//  Wristcord
+//
+//  Created by Brooks on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct WristcordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
