b498ee6fbc18579e93ecd0e7f9c029a8a8e3ea64
brooks !! <144816371+Brooks1228@users.noreply.github.com>
2025-10-20 14:19:37 -0400

    Update ContentView.swift

--- Wristcord Watch App/ContentView.swift
+++ Wristcord Watch App/ContentView.swift
@@ -7,11 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WaterTrackerView: View {
+    @State private var cups = 0
+    
     var body: some View {
+        VStack(spacing: 20){
+            Text("water tracker")
+                .font(.title)
+                .bold()
+            
+            Text("cups drunk: \(cups)")
+                .font(.title2)
+            
+            Button("drink Water") {
+                cups += 1
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            
+            Spacer()
+        }
+        .padding()
     }
 }
-
-#Preview {
-    ContentView()
-}
+    
+    struct ContentView: View {
+        var body: some View {
+            TabView {
+                WaterTrackerView()
+                    .tabItem {
+                        Label("Water", systemImage: "drop.fill")
+                    }
+            }
+//            TabView {
+//                        WaterTrackerView()
+//                            .tabItem {
+//                                Label("Water", systemImage: "drop.fill")
+//                            }
+//
+//                        HelloWorldView()
+//                            .tabItem {
+//                                Label("Hello", systemImage: "hand.wave")
+//                            }
+//                    }
+        }
+    }
+    
+    #Preview {
+        ContentView()
+    }
